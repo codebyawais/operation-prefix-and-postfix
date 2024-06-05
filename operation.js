1b71ada0d4a1728aaf09175970be88f46cb91c6b
WebDevByAwaisRafiq <webdevbyawais@gmail.com>
2024-06-05 07:23:49 +0500

    Add files via upload

---
+++ operation.js
@@ -0,0 +1,43 @@
+// let value = 4;
+// let negvalue = -value;
+// console.log(negvalue);
+
+// let value = -4;
+// let posvalue = value;
+// console.log(posvalue);
+
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2*2*3);
+// console.log(2/2);
+// console.log(2%2);
+
+
+
+//   string can be Add
+
+// let str1 ="awais";
+// let str2 = " rafiq";
+// let str3 = str1 + str2;
+// console.log(str3);
+ 
+
+
+// let num = 100;
+// let num2 = num++;
+// console.log(num);
+
+
+let num = 100;
+let num2 = +num;
+console.log(num);
+
+
+
+
+
+
+
